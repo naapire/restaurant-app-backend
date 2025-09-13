efcbba916e2ae39d1bf96c94dea3f4e548a0f9b2
Vida Naapire <vidanaapire@gmail.com>
2025-09-13 13:50:47 +0000

    corrected server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,10 @@
-import mysql, { Connection } from "mysql2";
-import dotenv from "dotenv";
+import express from 'express';
+import dotenv from 'dotenv'
+dotenv.config()
 
-dotenv.config();
+const port = process.env.SERVER_PORT
+const app = express();
+app.use(express.json());
 
-const connection: Connection = mysql.createConnection({
-  host: process.env.DB_HOST as string,
-  port: Number(process.env.DB_PORT),
-  user: process.env.DB_USER as string,
-  password: process.env.DB_PASSWORD as string,
-  database: process.env.DB_NAME as string,
-});
 
-// Test connection
-connection.connect((err) => {
-  if (err) {
-    console.error("❌ MySQL connection failed:", err.message);
-    return;
-  }
-  console.log("✅ MySQL Connected");
-});
-
-export default connection;
+app.listen(process.env.SERVER_PORT, () => console.log(`Server running on port http://localhost:${port}`));
